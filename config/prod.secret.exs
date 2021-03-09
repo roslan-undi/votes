62ff8bb8f4abf9e2719031fe1c6e58ae8cbb18a1
roslan-undi <80014764+roslan-undi@users.noreply.github.com>
2021-03-09 20:51:45 +0800

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,41 +1,18 @@
-# In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
 use Mix.Config
-
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
 config :votes, Votes.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+        host: "127.0.0.1",
+        port: 5432,
+        database: "votes",
+        username: "roslan",
+        password: "ramblingros",
+        pool_size: 10
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
 
 config :votes, VotesWeb.Endpoint,
-  http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: secret_key_base
-
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :votes, VotesWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+        server: true,
+        http: [port: 4000, transport_options: [sockets_opts: [:inet6]]],
+        check_origin: [
+                "https://undionline.com",
+                "http://undionline.com:4000
+        ],
+        secret_key_base: "2b2InLRoj+ywaPXfMc8Ik52+EHuqCHSuGH0LOPq7V1mahID3tKINefHvWRmkfMVE"
